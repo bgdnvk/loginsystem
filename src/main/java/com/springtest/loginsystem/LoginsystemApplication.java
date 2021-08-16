f82f5058868323b95227f4d047a823b2cd99c28c
bgdnvk <bnovykov@gmail.com>
2021-08-16 21:33:40 +0200

    init

---
+++ src/main/java/com/springtest/loginsystem/LoginsystemApplication.java
@@ -0,0 +1,13 @@
+package com.springtest.loginsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginsystemApplication.class, args);
+	}
+
+}
